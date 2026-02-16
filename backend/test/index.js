3d4b7537720d46a8a447ec2fa672637148b4d26a
Aditya singh <adityasingh14.2019@gmail.com>
2026-02-16 23:49:32 +0530

    **

--- backend/test/index.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById("but");
-btn.addEventListener("click", async () => {
-  const response = await fetch("https://api.restful-api.dev/objects", {
-    method: "GET",
-  });
-  const data = await response.json();
-  console.log(data);
-});
---
+++ error/index.js
@@ -0,0 +1,2 @@
+const arr = [{"id":1,"name":"aditya singh"}, {}, {}, {}, {}];
+console.log(arr[0].name);
