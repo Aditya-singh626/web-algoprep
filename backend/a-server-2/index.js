768fcd991ffe678e19f0b5b93dda68fae358280a
Aditya singh <adityasingh14.2019@gmail.com>
2026-02-19 16:22:17 +0530

    ****

--- backend/a-server-2/index.js
+++ backend/a-server-2/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // const cors = require("cors");
 const fs = require("fs");
+const { json } = require("stream/consumers");
 const { getCallSites } = require("util");
 const app = express();
 console.log("before");
@@ -21,12 +22,44 @@ function getAllPostsHandler(req, res) {
 
 function getbyId(req, res) {
   console.log("Recieved get Request");
+  const id = req.params.posteId;
+  const post = jsonPosts.posts[id - 1];
+  if (post) {
+    res.status(200).json(post);
+  }
+}
+function update(req, res) {
+  const id = req.params.posteId;
+  const title = req.body.head;
+  const bodytext = req.body.text;
+  const post = jsonPosts.posts[id-1];
+  if (post) {
+    post.title = title;
+    post.body = bodytext;
+    res.status(200).json(post);
+  }
+}
+function deletepost(req, res) {
+  const id = req.params.posteId;
+  if(id){
+    jsonPosts.posts.splice(id-1,1);
+    const postarr = jsonPosts.posts;
+    res.status(200).json({postarr});
+  }else{
+    json({
+      message:"post not found"
+    })
+  }
+}
 
-};
 // app.use(cors());
 // app.use(cors({ origin: "http://127.0.0.1:5500" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/posts", getAllPostsHandler);
-app.get("/posts/:posteId",getbyId);
+app.get("/posts/:posteId", getbyId);
+app.patch("/posts/:posteId", update);
+app.delete("/posts/:posteId",deletepost);
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
