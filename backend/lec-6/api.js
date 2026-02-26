57bd8160152e707680646f13eb2a938979bfbd5b
Aditya singh <adityasingh14.2019@gmail.com>
2026-02-26 23:00:50 +0530

    *****

--- backend/lec-6/api.js
+++ backend/lec-6/api.js
@@ -5,6 +5,6 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 dotenv.config();
 
-app.listen(3001, function () {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
