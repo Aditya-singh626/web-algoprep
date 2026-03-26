900da802ce723120878f6d203c1365a91b835f47
Aditya singh <adityasingh14.2019@gmail.com>
2026-03-26 18:23:03 +0530

    *****

--- java/test/index.js
+++
@@ -1,8 +0,0 @@
-let list = document.getElementById("add");
-let text = document.createElement("li");
-text.textContent = "New Item";
-list.appendChild(text);
-let button = document.createElement("button");
-button.textContent = "Add Item";
-button.id = "add-item-btn";
-text.appendChild(button);
